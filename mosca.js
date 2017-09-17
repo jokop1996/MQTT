5771239693698715e99f40416c5f3a056584f604
Tio <tiorezafk@yahoo.com>
2017-09-17 22:50:35 +0700

    response server

--- mosca.js
+++ mosca.js
@@ -1,9 +1,19 @@
 var http     = require('http')
-  , httpServ = http.createServer()
+  , httpServ = http.createServer((request,response) => {
+
+    response.writeHead(200);
+
+    response.write('Server Running');
+
+    response.end();
+
+  })
   , mosca    = require('mosca')
   , mqttServ = new mosca.Server({});
 
 mqttServ.attachHttpServer(httpServ);
 
 var port = process.env.PORT || 3000;
-httpServ.listen(port);
+httpServ.listen(port,(res) => {
+   console.log('Server listening on port ' + httpServ.address().port);
+});
