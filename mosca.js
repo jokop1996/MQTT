5f22e7375b0edecff788fe3235147820ae7776ad
jokop1996 <jokopurwanto1996@gmail.com>
2017-09-18 19:01:33 +0700

    Update mosca.js

--- mosca.js
+++ mosca.js
@@ -1,11 +1,9 @@
 var http     = require('http')
-  , httpServ = http.createServer()
-  , mosca    = require('mosca')
-  , mqttServ = new mosca.Server({});
+, httpServ = http.createServer()
+, mosca    = require('mosca')
+, mqttServ = new mosca.Server({});
 
 mqttServ.attachHttpServer(httpServ);
 
 var port = process.env.PORT || 3000;
-httpServ.listen(port,function() {
-   console.log('Server listening on port ' + httpServ.address().port);
-});
+httpServ.listen(port);
